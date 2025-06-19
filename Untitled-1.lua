a4b7676c0c466e911f69ee0fa00d0ba800023596
Shubham Patel <ptlshubham@gmail.com>
2025-06-19 14:51:20 +0530

    Rumit Code

---
+++ Untitled-1.lua
@@ -0,0 +1,22 @@
+Marketing
+
+
+
+file:///Users/rumit/Downloads/sandbox341/dist/demo19.html
+Lead Generation
+--
+
+file:///Users/rumit/Downloads/sandbox341/dist/demo35.html
+Email Marketing
+--
+
+file:///Users/rumit/Downloads/sandbox341/dist/demo22.html
+Brand management
+
+
+file:///Users/rumit/Downloads/sandbox341/dist/demo33.html
+Graphic Design
+--
+
+file:///Users/rumit/Downloads/sandbox341/dist/demo28.html
+Product Styling / Photographyw
